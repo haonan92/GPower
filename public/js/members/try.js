dcdc60c656a245f19f4e591ed0cdcd29439dcd74
GeePower <nvligpower@gmail.com>
2016-03-12 15:11:21 -0500

    done with members

---
+++ public/js/members/try.js
@@ -0,0 +1,18 @@
+﻿
+
+
+new Photostack( document.getElementById( 'photostack-1' ), {
+    callback : function( item ) {
+        //console.log(item)
+    }
+} );
+new Photostack( document.getElementById( 'photostack-2' ), {
+    callback : function( item ) {
+        //console.log(item)
+    }
+} );
+new Photostack( document.getElementById( 'photostack-3' ), {
+    callback : function( item ) {
+        //console.log(item)
+    }
+} );
